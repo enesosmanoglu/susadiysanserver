540356703fd2035700f78b29f3814c7e4d23c694
Enes Osmanoğlu <enesosmanoglu@gmail.com>
2021-09-23 01:32:19 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const fs = require("fs");
+const sanitize = require("sanitize-filename");
+const express = require("express");
+const app = express();
+const Busboy = require("busboy");
+
+app.use(function (req, res, next) {
+    // Anti-cors
+    req.header("Access-Control-Allow-Origin", "*");
+    req.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+app.use(express.static("./"));
+
+//# use alternate localhost and the port Heroku assigns to $PORT
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+let server = app.listen(port, host, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port);
+});
